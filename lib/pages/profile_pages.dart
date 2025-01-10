300b3756bf48d8e05fda4b0f83f4982d5df826bc
alyapr <putririzaldyalya@gmail.com>
2025-01-10 15:07:27 +0700

    menambah profile screen

--- lib/pages/profile_pages.dart
+++ lib/pages/profile_pages.dart
@@ -1,12 +1,256 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfilePage> {
+  //TODO : 1. Deklarasikan variabel yang dibutuhkan
+  bool isSignedIn = false;
+  String fullName = 'Alya Putri Rizaldy';
+  String userName = 'Alya Putri Rizaldy';
+  String email = 'putririzaldyalya@gmail.com';
+  int favoriteCatCount = 0;
+
+  //TODO 5. Implementasi fungsi SignIn
+  void signIn() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
+  //TODO 6. Implementasi fungsi SignOut
+  void signOut() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profile Page", style: TextStyle(fontSize: 24)),
-
-      // CINTA
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            color: Colors.cyan[400],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //TODO :2. Bagian profile header (isinya gambar profil)
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 150),
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.orange,
+                                width: 2,
+                              ),
+                              shape: BoxShape.circle),
+                          child: const CircleAvatar(
+                            radius: 50,
+                            backgroundImage:
+                                AssetImage('assets/images/catProfile.png'),
+                          ),
+                        ),
+                        if (isSignedIn)
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.camera_alt,
+                                color: Colors.deepPurple[50],
+                              ))
+                      ],
+                    ),
+                  ),
+                ),
+                // TODO : 3. Buat bagian profile info (isinya info profile)
+                // Baris satu profile info username
+                SizedBox(
+                  height: 20,
+                ),
+                Divider(
+                  color: Colors.deepPurple[100],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.lock,
+                            color: Colors.amber,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Pengguna',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        ': $userName',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+                // Baris kedua profile info nama
+                SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  color: Colors.deepPurple[100],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.person,
+                            color: Colors.blue,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Nama',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                        child: Text(
+                      ': $fullName',
+                      style: const TextStyle(fontSize: 18),
+                    )),
+                    if (isSignedIn) const Icon(Icons.edit),
+                  ],
+                ),
+                // Baris ketiga profile info Email
+                SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  color: Colors.deepPurple[100],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.email,
+                            color: Colors.black,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Email',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                        child: Text(
+                      ': $email',
+                      style: const TextStyle(fontSize: 18),
+                    )),
+                    if (isSignedIn) const Icon(Icons.edit),
+                  ],
+                ),
+                // Baris keempat favorit
+                const SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  color: Colors.deepPurple[100],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Favorit',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        ': $favoriteCatCount',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+                //TODO : 4. Buat bagian profile action (isinya tombol signin/signout)
+                const SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  color: Colors.deepPurple[100],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                isSignedIn
+                    ? TextButton(onPressed: signOut, child: Text('Sign Out'))
+                    : TextButton(
+                        onPressed: signIn, child: const Text('Sign in'))
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
